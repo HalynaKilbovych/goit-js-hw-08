b764c35ccd08f077f0477e1b148cc6857c7b4bcd
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-12-04 01:17:38 +0200

    2, 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,21 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let time = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if(time != null) {
+    player.setCurrentTime(time);
+}
+
+player.on('timeupdate', throttle(saveTimePlayer),1000);
 
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-  
-// player.on('eventName', function(data) {
-//     // data is an object containing properties specific to that event
-// });
+function saveTimePlayer () {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = { 
+    form: document.querySelector('.feedback-form'), 
+    email: document.querySelector('.feedback-form input'), 
+    textarea: document.querySelector('.feedback-form textarea'), 
+}; 
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+
+refs.form.addEventListener('submit', onFormSubmit); 
+refs.form.addEventListener('input',throttle(onInput, 500));;
+
+
+function onInput(e) { 
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY , JSON.stringify(formData));
+
+};
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))); 
+    e.currentTarget.reset(); 
+    localStorage.removeItem(LOCAL_STORAGE_KEY)
+
+};  
+
+populateData(); 
+function populateData() {
+    const parseData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (parseData) {
+        console.log(parseData);
+      refs.textarea.value = parseData.message;
+      refs.email.value = parseData.email;
+    }
+  }
+  
