564aaab6891c372b383b6912e35378c4e39d355d
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-12-03 20:26:13 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+// import Player from '@vimeo/player';
 
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+  
+// player.on('eventName', function(data) {
+//     // data is an object containing properties specific to that event
+// });
